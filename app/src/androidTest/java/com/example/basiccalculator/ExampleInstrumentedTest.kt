c1b6d5b3d4e7cd0bb3fd9eedccb4fa1f9a7a34e2
Rajeev Rahi <rajeev.rahi32@gmail.com>
2024-05-11 11:13:30 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/basiccalculator/ExampleInstrumentedTest.kt
@@ -0,0 +1,35 @@
+package com.example.basiccalculator
+
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @get:Rule
+    var rule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.basiccalculator.debug", appContext.packageName)
+    }
+    @Test
+    fun launchCalculatorApp() {
+        rule.onNodeWithTag("first_number").assertExists()
+        Thread.sleep(5000)
+    }
+}
